deddfc10068c6f418cc76dfccd56cacffc2a104f
Sanjeev Mishra <sanjeev@codemancers.com>
2012-05-10 00:33:37 +0530

    fixed a bug

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :domain => "whiteapple.com",
+    :domain => "scrumkook.com",
     :user_name => "sanjusoftware",
-    :password => "sanjusadhna",
+    :password => "yourpassword",
     :authentication => "plain",
     :enable_starttls_auto => true
 }
